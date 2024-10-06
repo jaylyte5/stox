7186fc56d5605259beeafcd058b5fa9e3b8da53a
Emerson <emersonhicks003@gmail.com>
2024-10-06 00:47:34 -0400

    theme switcharoo thing

--- src/app/app/settings/page.tsx
+++ src/app/app/settings/page.tsx
@@ -1,5 +1,30 @@
+'use client';
+
+import { useEffect } from 'react'
+import { themeChange } from 'theme-change'
+
 export default function SettingsPage() {
+
+    useEffect(() => {
+        themeChange(false)
+        // 👆 false parameter is required for react project
+
+
+
+    }, [])
+
     return (
-        <h1>Settings page!</h1>
+        <div>
+            <h1>Theme selector</h1>
+            <select data-choose-theme>
+                <option value="light">Light</option>
+                <option value="dark">Dark</option>
+                <option value="cyberpunk">Cyberpunk</option>
+                <option value="aqua">Aqua</option>
+                <option value="lofi">Lofi</option>
+                <option value="coffee">Coffee</option>
+                <option value="valentine">Valentine</option>
+            </select>
+        </div>
     )
 }
