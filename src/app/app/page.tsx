1d164cf88e6bfb98d7680e8b7b9f4868dbaaec0e
Emerson <emersonhicks003@gmail.com>
2024-10-09 21:21:27 -0400

    Create SocialFeed.tsx

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,9 +1,11 @@
+import SocialFeed from "@/components/Widgets/SocialFeed";
 import WatchList from "@/components/Widgets/WatchList";
 
 export default function Dashboard() {
     return (
-        <div className=" grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <div className="grid grid-cols-1 gap-20 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             <WatchList />
+            <SocialFeed />
         </div>
     )
 }
